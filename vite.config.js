979d66d48ce2a5ab5adba91444d789111b11d9af
cmraipottasiri <105407342+cmraipottasiri@users.noreply.github.com>
2025-07-22 10:57:31 +0530

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-(property) default: UserConfig;
+
+// https://vite.dev/config/
 export default defineConfig({
-  base:'/gitactionex/',
   plugins: [react()],
 })
